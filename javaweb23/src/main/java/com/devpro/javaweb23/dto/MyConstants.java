58cb9e090fa2ad9f1e8f8adf73486ca1a52a588d
dacduy2402 <dacduy2402@gmail.com>
2023-04-16 11:28:35 +0700

    private email account

--- javaweb23/src/main/java/com/devpro/javaweb23/dto/MyConstants.java
+++ javaweb23/src/main/java/com/devpro/javaweb23/dto/MyConstants.java
@@ -2,11 +2,11 @@ package com.devpro.javaweb23.dto;
 
 public class MyConstants {
     // Replace with your email here:  
-    public static final String MY_EMAIL = "dacduy2402@gmail.com";
+    public static final String MY_EMAIL = "";
 
     // Replace password!!
-    public static final String MY_PASSWORD = "wltoihtjwpthbcho";
+    public static final String MY_PASSWORD = "";
 
     // And receiver!
-//    public static final String FRIEND_EMAIL = "khuongduy198@gmail.com";
+//    public static final String FRIEND_EMAIL = "";
 }
